500f10480fe7d2ba7beab7ad223c0450ecad61ac
Daniel <dvillaj@gmail.com>
2018-02-13 15:29:50 +0100

    secret

--- comun/secret.py
+++ comun/secret.py
@@ -1,5 +1,5 @@
 # Variables that contains the user credentials to access Twitter API 
-consumer_key = 'blMK8QFgzF3ms2niIEABs9wzO'
-consumer_secret = 'lEWIPiGBczXZutR0JgA1sxJzafKmLbnC4DmsA4kCWWyog7ox3o'
-access_token = '12391902-0bZCGZcLcnYIASAGxVbzB2rOMpAcAA1wG1QfkLdzH'
-access_token_secret = '6x5lXv9WbIBrsFgzzoQ8vpLr7NOQ2agy0lDlGBoTaTJua'
+consumer_key = 'CONSUMER_KEY'
+consumer_secret = 'CONSUMER_SECRET'
+access_token = 'ACCESS_TOKEN'
+access_token_secret = 'ACCESS_SECRET'
